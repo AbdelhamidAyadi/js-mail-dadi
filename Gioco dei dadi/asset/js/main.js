880bccd86875310b6d704859ba90e8aa3259b10b
AbdelhamidAyadi <Abdelhamid2002ayadi@gmail.com>
2022-03-15 23:06:07 +0100

    Esercizio: Gioco dei dadi.

--- Gioco dei dadi/asset/js/main.js
+++ Gioco dei dadi/asset/js/main.js
@@ -1,1 +1,22 @@
-let 
+//comunque non so se è corretto il esercizio ,il problema che non ho capito bene cosa devo fare ,ho provato a fare quello che ho capito.
+
+let container = document.getElementById("container");
+let dado = [1 ,2 ,3 ,4 ,5 ,6];
+
+let randomComputer = Math.floor(Math.random() *5 );
+let randomUser = Math.floor(Math.random() *5 );
+
+let dadoRisultatoComputer = dado[randomComputer];
+let dadoRisultatoUser = dado[randomUser];
+
+console.log(dadoRisultatoComputer);
+console.log(dadoRisultatoUser);
+
+
+if( dadoRisultatoComputer > dadoRisultatoUser){
+    container.innerHTML = "Hai perso!" ;
+}else if( dadoRisultatoComputer == dadoRisultatoUser ){
+    container.innerHTML = "Pareggio!" ;
+}else if (dadoRisultatoComputer < dadoRisultatoUser){
+    container.innerHTML = "Hai vinto!" ;
+}
